562bb1dff57681d1a611d8c9a989d47f1d7c4cbe
autotiger <menghu96@gmail.com>
2018-03-21 22:45:03 -0700

    added LC19

---
+++ src/hmeng/lc/medium/LC0019.java
@@ -0,0 +1,38 @@
+package hmeng.lc.medium;
+
+import hmeng.lc.common.ListNode;
+
+/*
+19. Remove Nth Node From End of List
+
+Given a linked list, remove the nth node from the end of list and return its head.
+
+For example,
+
+   Given linked list: 1->2->3->4->5, and n = 2.
+
+   After removing the second node from the end, the linked list becomes 1->2->3->5.
+Note:
+Given n will always be valid.
+Try to do this in one pass.
+ */
+public class LC0019 {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode first = dummy, second = dummy;
+        while (n >= 0) {
+            first = first.next;
+            n--;
+        }
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        
+        second.next = second.next.next;
+        return dummy.next;
+    }
+    
+}
