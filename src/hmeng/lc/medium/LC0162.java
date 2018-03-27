c898cffc6868f5a52fdb8217fbbfd131557d8493
autotiger <menghu96@gmail.com>
2018-03-26 23:15:58 -0700

    added binary search for LC162

--- src/hmeng/lc/medium/LC0162.java
+++ src/hmeng/lc/medium/LC0162.java
@@ -14,6 +14,19 @@ You may imagine that num[-1] = num[n] = -inf.
 For example, in array [1, 2, 3, 1], 3 is a peak element and your function should return the index number 2.
 */
 public class LC0162 {
+    
+/*    public int findPeakElement(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] > nums[mid + 1])
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        return l;
+    }*/
+    
     public int findPeakElement(int[] nums) {
         for (int i = 0; i < nums.length-1; i++) {
             if (nums[i] > nums[i+1]) return i;
