5daf37f90d0d9707d9f57d774371a5171ef42237
autotiger <menghu96@gmail.com>
2018-03-31 14:13:47 -0700

    added LC422 and updated some comments

--- src/hmeng/lc/easy/LC0463.java
+++ src/hmeng/lc/easy/LC0463.java
@@ -2,7 +2,7 @@ package hmeng.lc.easy;
 
 /*
 463. Island Perimeter
-DescriptionHintsSubmissionsDiscussSolution
+
 You are given a map in form of a two-dimensional integer grid where 1 represents land and 0 represents water. 
 Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, 
 and there is exactly one island (i.e., one or more connected land cells). The island doesn't have "lakes" (water 
--- src/hmeng/lc/hard/LC0269.java
+++ src/hmeng/lc/hard/LC0269.java
@@ -9,7 +9,7 @@ import java.util.Set;
 
 /*
 269. Alien Dictionary
-DescriptionHintsSubmissionsDiscussSolution
+
 There is a new alien language which uses the latin alphabet. However, the order among letters are unknown to you. 
 You receive a list of non-empty words from the dictionary, where words are sorted lexicographically by the rules of this new language.
 Derive the order of letters in this language.
